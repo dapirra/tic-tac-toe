3c3a373f84c7ff61f463112bc86b18edf232c3ad
dapirra <35818038+dapirra@users.noreply.github.com>
2021-04-14 02:56:30 -0400

    feat: Added basic Tic-Tac-Toe functionality

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,12 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      <button>{{value}}</button>
-    </p>
+    <button>{{value}}</button>
   `,
-  styles: [
-  ]
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O';
