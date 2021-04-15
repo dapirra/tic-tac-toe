b53f7b1f1d267315e3a70d30bcd72c31d8308bb1
dapirra <35818038+dapirra@users.noreply.github.com>
2021-04-15 18:42:28 -0400

    doc:board: Added comments to each winning line

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -37,14 +37,14 @@ export class BoardComponent implements OnInit {
 
   calculateWinner() {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
+      [0, 1, 2], // Top Row
+      [3, 4, 5], // Middle Row
+      [6, 7, 8], // Bottom Row
+      [0, 3, 6], // Left Column
+      [1, 4, 7], // Middle Column
+      [2, 5, 8], // Right Column
+      [0, 4, 8], // Back Diagonal \
+      [2, 4, 6], // Forward Diagonal /
     ];
 
     for (let i = 0; i < lines.length; i++) {
