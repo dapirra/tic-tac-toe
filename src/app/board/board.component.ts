c08a321071c8b078414b34ff25868cbdd641b3ca
dapirra <35818038+dapirra@users.noreply.github.com>
2021-04-19 16:38:02 -0400

    refact:board: Changed calculate winner code

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -10,12 +10,22 @@ export class BoardComponent implements OnInit {
   xIsNext: boolean;
   winner: string;
   movesLeft: number;
-  winningLine: number[];
+  private static winningLines: number[][];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.newGame();
+    BoardComponent.winningLines = [
+      [0, 1, 2], // Top Row
+      [3, 4, 5], // Middle Row
+      [6, 7, 8], // Bottom Row
+      [0, 3, 6], // Left Column
+      [1, 4, 7], // Middle Column
+      [2, 5, 8], // Right Column
+      [0, 4, 8], // Back Diagonal \
+      [2, 4, 6], // Forward Diagonal /
+    ];
   }
 
   newGame() {
@@ -23,7 +33,6 @@ export class BoardComponent implements OnInit {
     this.winner = null;
     this.xIsNext = true;
     this.movesLeft = 9;
-    this.winningLine = null;
   }
 
   get player() {
@@ -42,39 +51,24 @@ export class BoardComponent implements OnInit {
       this.movesLeft--; // Subtract from the total number of moves remaining
     }
 
-    this.winner = this.calculateWinner();
-    if (this.winner) {
-      for (let index of this.winningLine) {
-        this.squares[index] = this.squares[index] + '!';
-      }
-    }
+    this.calculateWinner();
   }
 
   calculateWinner() {
-    const lines = [
-      [0, 1, 2], // Top Row
-      [3, 4, 5], // Middle Row
-      [6, 7, 8], // Bottom Row
-      [0, 3, 6], // Left Column
-      [1, 4, 7], // Middle Column
-      [2, 5, 8], // Right Column
-      [0, 4, 8], // Back Diagonal \
-      [2, 4, 6], // Forward Diagonal /
-    ];
-
     // Loop through all win cases to see if a player won
-    for (let line of lines) {
+    for (let line of BoardComponent.winningLines) {
       const [a, b, c] = line;
       if ( // @-- Make sure the square is defined; can't have 3 nulls in a row
         this.squares[a] && // <--@
         this.squares[a] === this.squares[b] &&
         this.squares[a] === this.squares[c]
-      ) {
-        this.winningLine = line;
-        return this.squares[a];
+      ) { // When there is 3 in a row...
+        this.winner = this.squares[a]; // Set the winner
+        for (let index of line) { // Mark the 3 that are in a row
+          this.squares[index] += '!';
+        }
+        return;
       }
     }
-
-    return null;
   }
 }
