c33bc147b30a361e4235434c49974d458e512056
dapirra <35818038+dapirra@users.noreply.github.com>
2021-04-27 05:30:35 -0400

    mod: Diagonals will be checked for a win first

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -24,14 +24,14 @@ export class BoardComponent implements OnInit {
   ngOnInit(): void {
     this.newGameOptions();
     BoardComponent.winningLines = [
+      [0, 4, 8], // Back Diagonal \
+      [2, 4, 6], // Forward Diagonal /
       [0, 1, 2], // Top Row
       [3, 4, 5], // Middle Row
       [6, 7, 8], // Bottom Row
       [0, 3, 6], // Left Column
       [1, 4, 7], // Middle Column
       [2, 5, 8], // Right Column
-      [0, 4, 8], // Back Diagonal \
-      [2, 4, 6], // Forward Diagonal /
     ];
   }
 
