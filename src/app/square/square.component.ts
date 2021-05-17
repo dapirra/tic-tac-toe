efab63f8ca400d8d900384a03ccc68c8ba1c9edd
dapirra <35818038+dapirra@users.noreply.github.com>
2021-05-17 17:26:41 -0400

    refact:square: Moved scss file to inline section

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -10,7 +10,19 @@ import { Component, Input } from '@angular/core';
         {{ value?.charAt(0) }}
     </button>
   `,
-  styleUrls: ['./square.component.scss']
+  styles: [`
+    @for $i from 0 through 8 {
+      button#square#{$i} {
+        width: 100%;
+        height: 100%;
+        transition-property: none;
+        font-size: 5em;
+        background-color: #323259;
+        border: 0;
+      }
+    }
+  `]
+  // styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O' | 'X!' | 'O!';
